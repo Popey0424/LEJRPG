cdd61237bd9659de2f7a9cc3ae2cddd1b349f609
DESKTOP-N4F36O6\roylo <roylouis0424@gmail.com>
2024-03-12 21:35:41 +0100

    Main MEnu scripts

---
+++ Assets/Scripts/MainMenu/MenuManager.cs
@@ -0,0 +1,143 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+using UnityEngine.Audio;
+
+public class MainMenuManger : MonoBehaviour
+{
+    public AudioMixer MixerSFX;
+    public AudioMixer MixerVolume;
+
+
+    [SerializeField] private Image imageFade;
+    [SerializeField] private GameObject menuOptions;
+    [SerializeField] private GameObject menuCredits;
+    private AudioSource Music;
+    public AudioSource ClickMenu;
+
+
+    //[Header("Animation Credits")]
+    //public GameObject Background;
+    //public GameObject Grid;
+    //public GameObject Back_Button;
+
+
+
+
+    public void OnClickPlay()
+    {
+        ClickMenu.Play();
+        imageFade.gameObject.SetActive(true);
+        imageFade.DOFade(1, 2.9f).OnComplete(FadeComplete);
+
+
+    }
+
+    private void FadeComplete()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void OnClickOptions()
+    {
+        ClickMenu.Play();
+        menuOptions.SetActive(true);
+
+
+
+    }
+
+
+    public void OnClickExit()
+    {
+        ClickMenu.Play();
+        menuOptions.SetActive(false);
+        menuCredits.SetActive(false);
+    }
+
+
+    public void OnClickCredits()
+    {
+
+        ////Animator Credits
+        //Animator animator_Credits = Background.GetComponent<Animator>();
+        //if (animator_Credits != null)
+        //{
+        //    bool Isopen = animator_Credits.GetBool("Open");
+
+        //    animator_Credits.SetBool("Open", !Isopen);
+        //}
+
+
+        ////Animator Grid
+        //Animator animator_Grid = Grid.GetComponent<Animator>();
+        //if (animator_Grid != null)
+        //{
+        //    bool Isopen_Grid = animator_Grid.GetBool("Open_Grid");
+
+        //    animator_Grid.SetBool("Open_Grid", !Isopen_Grid);
+        //}
+
+        //// Animator BackButton
+        //Animator animator_BackButton = Back_Button.GetComponent<Animator>();
+        //if (animator_BackButton != null)
+        //{
+
+        //    bool Isopen_BackButton = animator_BackButton.GetBool("Open_BackButton");
+        //    animator_BackButton.SetBool("Open_BackButton", !Isopen_BackButton);
+        //}
+
+        //print("terminé2");
+        //imageFade.gameObject.SetActive(true);
+        //imageFade.DOFade(1, 2.9f).OnComplete(FadeCompleteCredits);
+
+    }
+    public void FadeCompleteCredits()
+    {
+        menuCredits.SetActive(true);
+        imageFade.gameObject.SetActive(false);
+    }
+
+    public void OnClickQuit()
+    {
+        ClickMenu.Play();
+        Application.Quit();
+    }
+
+    public void OnMusicValueChanged(float newValue)
+    {
+        Music.volume = newValue;
+
+    }
+
+
+    public void OnSFXValueChanged(float newValue)
+    {
+        if (newValue < 0.01f)
+        {
+            newValue = 0.01f;
+        }
+
+        float volume = Mathf.Log10(newValue) * 20;
+        PlayerPrefs.SetFloat("SFX_Volume", newValue);// permet de recuper apres les preference du player en change Set par Get. 
+        MixerSFX.SetFloat("SFX_Volume", volume);
+    }
+
+    public void OnVolumeValueChanges(float newValue)
+    {
+        if (newValue < 0.01f)
+        {
+            newValue = 0.01f;
+        }
+        float volume = Mathf.Log10(newValue) * 20;
+
+        MixerVolume.SetFloat("Volume_Volume", volume);
+    }
+
+
+
+}
---
+++ Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+using UnityEngine.Audio;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    [SerializeField] private GameObject MenuPause;
+    [SerializeField] private Image ImageFade;
+    public GameObject OptionsPauseMenu;
+    private AudioSource Music;
+    public AudioMixer MixerSFX;
+    public AudioMixer MixerVolume;
+    public Rigidbody2D rigiplayer;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (!MenuPause.activeSelf)
+            {
+                Time.timeScale = 0f;
+                MenuPause.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1f;
+                MenuPause.SetActive(false);
+
+
+            }
+            //GetComponent<GameCore>().m_timerClick = 0;
+            //Time.timeScale = 0f;
+            //MenuPause.SetActive(true);
+        }
+    }
+
+
+    public void OnClickResume()
+    {
+        MenuPause.SetActive(false);
+        Time.timeScale = 1f;
+        //GetComponent<GameCore>().m_timerClick = 0;
+    }
+
+    public void OnClickLeave()
+    {
+        ImageFade.DOFade(1, 2.9f).OnComplete(FadeComplete);
+        Time.timeScale = 1f;
+    }
+
+    public void FadeComplete()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void OnClickExit()
+    {
+        Time.timeScale = 1f;
+        Application.Quit();
+    }
+
+    public void OnClickOptions()
+    {
+        Time.timeScale = 1f;
+        OptionsPauseMenu.SetActive(true);
+        // cherker si le 0 ca marche 
+
+    }
+
+    public void OnMusicValueChanged(float newValue)
+    {
+        Music.volume = newValue;
+
+    }
+
+    public void OnClickBackOptions()
+    {
+        OptionsPauseMenu.SetActive(false);
+    }
+
+    public void OnSFXValueChanged(float newValue)
+    {
+        if (newValue < 0.01f)
+        {
+            newValue = 0.01f;
+        }
+
+        float volume = Mathf.Log10(newValue) * 20;
+        PlayerPrefs.SetFloat("SFX_Volume", newValue);// permet de recuper apres les preference du player en change Set par Get. 
+        MixerSFX.SetFloat("SFX_Volume", volume);
+    }
+
+    public void OnVolumeValueChanges(float newValue)
+    {
+        if (newValue < 0.01f)
+        {
+            newValue = 0.01f;
+        }
+        float volume = Mathf.Log10(newValue) * 20;
+
+        MixerVolume.SetFloat("Volume_Volume", volume);
+    }
+
+
+}
